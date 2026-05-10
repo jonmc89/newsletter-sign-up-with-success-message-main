038456dcd9af770753549953bab205247f6e73d0
Jon <126274870+jonmc89@users.noreply.github.com>
2026-05-10 17:19:33 +0100

    feat: :sparkles: Added success page

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
 const error = document.getElementById('error');
-
+const successScreen = document.getElementById('successScreen');
+const dismissBtn = document.getElementById('dismissBtn');
+const card = document.getElementById('card');
 const emailRegExp = /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)*$/i;
 
 const isValidEmail = () => {
@@ -16,6 +18,8 @@ const setEmailClass = (isValid) => {
 const updateError = (isValid) => {
   if (isValid) {
     error.textContent = '';
+    successScreen.classList.add('success-screen-visible');
+    card.classList.add('hide-form');
   } else {
     error.textContent = 'Valid email required';
   }
@@ -29,3 +33,8 @@ const handleSubmit = (event) => {
 };
 
 form.addEventListener('submit', handleSubmit);
+
+dismissBtn.addEventListener('click', () => {
+  successScreen.classList.remove('success-screen-visible');
+  card.classList.remove('hide-form');
+});
